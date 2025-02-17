141f7262c2a49c2cdfd7fa46be5d53bdb14f608d
Aryaan007-sudo <blankgaming75@gmail.com>
2025-02-17 17:16:16 +0530

    done

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,11 @@
 const express = require('express')
-const {createUser,getAllUser} = require('../controller/userController')
+const {createUser,getAllUser,delteUserById,updateUserById,deleteAll} = require('../controller/userController')
 const router = express.Router()
 
 router.get('/',getAllUser)
 router.post('/',createUser)
+router.delete('/',deleteAll)
+router.delete('/:id',delteUserById)
+router.put('/:id',updateUserById)
 
 module.exports = router;
